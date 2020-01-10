cd6613f84f5ffd6cac0c0daaea28aa180bd63aa6
mmicciolo <mmicciolo@wpi.edu>
2020-01-10 18:51:58 -0500

    Commiting rest of inital commit.

---
+++ src/test/java/org/wlcp/wlcptranspiler/WlcpTranspilerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wlcp.wlcptranspiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlcpTranspilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
