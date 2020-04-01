6f396f9468b013add1b7b74c77626a094be555bf
mmicciolo <mmicciolo@wpi.edu>
2020-03-31 21:08:01 -0400

    Fixed issue with feignclient when running locally.

--- src/main/java/org/wlcp/wlcptranspiler/feignclient/GameFeignClient.java
+++ src/main/java/org/wlcp/wlcptranspiler/feignclient/GameFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.wlcp.wlcptranspiler.dto.GameDto;
 
-@FeignClient(contextId="game-client", name="wlcp-api")
+@FeignClient(contextId="game-client", name="wlcp-api", url="${wlcp-api-url:}")
 public interface GameFeignClient {
 	
     @RequestMapping(method=RequestMethod.GET, value="/gameController/getGame/{gameId}")
