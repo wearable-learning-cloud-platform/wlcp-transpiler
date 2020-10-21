b65aab86252211c100614016db2adc1a95d53a89
mmicciolo <mmicciolo@wpi.edu>
2020-10-21 19:02:35 -0400

    Implemented more error handling and lots of new security.

--- src/main/java/org/wlcp/wlcptranspiler/feignclient/GameFeignClient.java
+++ src/main/java/org/wlcp/wlcptranspiler/feignclient/GameFeignClient.java
@@ -2,6 +2,7 @@ package org.wlcp.wlcptranspiler.feignclient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.wlcp.wlcptranspiler.dto.GameDto;
@@ -10,6 +11,6 @@ import org.wlcp.wlcptranspiler.dto.GameDto;
 public interface GameFeignClient {
 	
     @RequestMapping(method=RequestMethod.GET, value="/gameController/getGame/{gameId}")
-    GameDto getGame(@PathVariable String gameId);
+    GameDto getGame(@PathVariable String gameId, @RequestHeader("Authorization") String bearerToken);
 
 }
---
+++ src/main/java/org/wlcp/wlcptranspiler/security/SecurityConstants.java
@@ -0,0 +1,7 @@
+package org.wlcp.wlcptranspiler.security;
+
+public class SecurityConstants {
+	
+	 public static final String JWT_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIifQ.CFsDr9KnYtLCfXkvDyyDyEUdnu5RPPyFQ32jiKMQ8NsbmTfMwVmIeWO0AJxYs8uyPP4txkvy4sP4T6asVN5cIw";
+	 
+}
