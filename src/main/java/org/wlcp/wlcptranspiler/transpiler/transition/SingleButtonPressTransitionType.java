5733703c1af430ad1d9e39da23527f190423bfd0
mmicciolo <mmicciolo@wpi.edu>
2021-12-26 19:11:50 -0500

    Implemented feature wlcp-15

--- src/main/java/org/wlcp/wlcptranspiler/transpiler/transition/SingleButtonPressTransitionType.java
+++ src/main/java/org/wlcp/wlcptranspiler/transpiler/transition/SingleButtonPressTransitionType.java
@@ -1,6 +1,7 @@
 package org.wlcp.wlcptranspiler.transpiler.transition;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -16,12 +17,26 @@ public class SingleButtonPressTransitionType extends TransitionType implements I
 	public String GenerateTranstion(String scope, Map<Connection, Transition> connectionTransitions, List<OutputState> outputStates, GameDto game) {
 		StringBuilder stringBuilder = new StringBuilder();
 		Map<String, String> buttonMap = GenerateButtonMap(scope, connectionTransitions, outputStates);
+		Map<String, String> labelMap = GenerateLabelMap(scope, connectionTransitions);
 		if(buttonMap.size() > 0) {
 			stringBuilder.append(GenerateTransitionConditional(scope));
-			stringBuilder.append(GenerateTransitionSingleButtonPress(scope, buttonMap));
+			stringBuilder.append(GenerateTransitionSingleButtonPress(scope, buttonMap, labelMap));
 			stringBuilder.append(GenerateTransitionEndConditional(scope));
 		}
 		return stringBuilder.toString();
+	}
+	
+	public Map<String, String> GenerateLabelMap(String scope, Map<Connection, Transition> connectionTransitions) { 
+		Map<String, String> labelMap = new LinkedHashMap<String, String>();
+		for(Entry<Connection, Transition> entry : connectionTransitions.entrySet()) {
+			if(entry.getValue().getSingleButtonPresses().containsKey(scope)) {
+				labelMap.put("label1", entry.getValue().getSingleButtonPresses().get(scope).getButton1Label());
+				labelMap.put("label2", entry.getValue().getSingleButtonPresses().get(scope).getButton2Label());
+				labelMap.put("label3", entry.getValue().getSingleButtonPresses().get(scope).getButton3Label());
+				labelMap.put("label4", entry.getValue().getSingleButtonPresses().get(scope).getButton4Label());
+			}
+		}
+		return labelMap;
 	}
 	
 	public Map<String, String> GenerateButtonMap(String scope, Map<Connection, Transition> connectionTransitions, List<OutputState> outputStates) {
@@ -45,7 +60,7 @@ public class SingleButtonPressTransitionType extends TransitionType implements I
 		return buttonMap;
 	}
 	
-	public String GenerateArrays(Map<String, String> buttonMap) {
+	public String GenerateArrays(Map<String, String> buttonMap, Map<String, String> labelMap) {
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("[");
 		int count = 0;
@@ -60,22 +75,31 @@ public class SingleButtonPressTransitionType extends TransitionType implements I
 		count = 0;
 		for(Entry<String, String> entry : buttonMap.entrySet()) {
 			if(count == buttonMap.size() - 1) {
-				stringBuilder.append("states." + entry.getValue() + "]");
+				stringBuilder.append("states." + entry.getValue() + "], [");
 			} else {
 				stringBuilder.append("states." + entry.getValue() + ", ");
+			}
+			count++;
+		}
+		count = 0;
+		for(Entry<String, String> entry : labelMap.entrySet()) {
+			if(count == labelMap.size() - 1) {
+				stringBuilder.append("\"" + entry.getValue() + "\"]");
+			} else {
+				stringBuilder.append("\"" + entry.getValue() + "\", ");
 			}
 			count++;
 		}
 		return stringBuilder.toString();
 	}
 	
-	private String GenerateTransitionSingleButtonPress(String scope, Map<String, String> buttonMap) {
+	private String GenerateTransitionSingleButtonPress(String scope, Map<String, String> buttonMap, Map<String, String> labelMap) {
 		StringBuilder stringBuilder = new StringBuilder();
-		GenerateArrays(buttonMap);
+		GenerateArrays(buttonMap, labelMap);
 		if(scope.equals("Game Wide")) {
-			stringBuilder.append("      this.state = this.playerVM.SingleButtonPress(" + GenerateArrays(buttonMap) + ");\n");
+			stringBuilder.append("      this.state = this.playerVM.SingleButtonPress(" + GenerateArrays(buttonMap, labelMap) + ");\n");
 		} else {
-			stringBuilder.append("         this.state = this.playerVM.SingleButtonPress(" + GenerateArrays(buttonMap) + ");\n");
+			stringBuilder.append("         this.state = this.playerVM.SingleButtonPress(" + GenerateArrays(buttonMap, labelMap) + ");\n");
 		}
 		return stringBuilder.toString();
 	}
