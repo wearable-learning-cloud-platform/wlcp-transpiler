fea5f639d121dbd9245db9d3ce608d7fd4b48e8d
mmicciolo <mmicciolo@wpi.edu>
2022-11-23 18:04:08 -0500

    Fixing wlcp-265

--- src/main/java/org/wlcp/wlcptranspiler/controller/TranspilerController.java
+++ src/main/java/org/wlcp/wlcptranspiler/controller/TranspilerController.java
@@ -1,6 +1,7 @@
 package org.wlcp.wlcptranspiler.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.wlcp.wlcptranspiler.dto.GameDto;
 import org.wlcp.wlcptranspiler.feignclient.GameFeignClient;
-import org.wlcp.wlcptranspiler.security.SecurityConstants;
 import org.wlcp.wlcptranspiler.serivce.impl.JavaScriptTranspilerServiceImpl;
 
 @Controller
@@ -21,14 +21,17 @@ public class TranspilerController {
 	
 	@Autowired
 	private JavaScriptTranspilerServiceImpl javaScriptTranspilerServiceImpl;
+	
+	@Value("${security.jwt-token}")
+	private String jwtToken;
 
 	@GetMapping(value="/transpileGame", produces="application/javascript")
 	public ResponseEntity<String> transpile(@RequestParam("gameId") String gameId, @RequestParam("archivedGame") boolean archivedGame) {
 		GameDto gameDto = null;
 		if(!archivedGame) {
-			gameDto = gameFeignClient.getGame(gameId, SecurityConstants.JWT_TOKEN);
+			gameDto = gameFeignClient.getGame(gameId, jwtToken);
 		} else {
-			gameDto = gameFeignClient.getArchivedGame(gameId, SecurityConstants.JWT_TOKEN);
+			gameDto = gameFeignClient.getArchivedGame(gameId, jwtToken);
 		}
 		return new ResponseEntity<String>(javaScriptTranspilerServiceImpl.transpile(gameDto), HttpStatus.OK);
 	}
